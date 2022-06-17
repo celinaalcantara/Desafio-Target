7a9cdf9e6db5e3667ba8b11b7a9e0161ee3ef542
Celina Alcantara <celinaacarmo@gmail.com>
2022-06-17 18:41:49 -0300

    questao 2

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,77 @@
+fun main(args: Array<String>) {
+
+
+    /*2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos
+    2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem
+    que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem
+    avisando se o número informado pertence ou não a sequência.*/
+
+    /*println("Insira um número:\n")
+    var numInserido = readLine()!!.toInt()
+
+    var num1 = 0
+    var num2 = 1
+    var numF = 0
+
+   while (numInserido > numF){
+        numF = num1 + num2
+        num1 = num2
+        num2 = numF
+    }
+
+    if (numInserido == numF){
+        return println("O numero $numInserido pertence à sequência de Fibonacci")
+
+    }else{
+        return println("O numero $numInserido não pertence à sequência de Fibonacci")
+    }*/
+
+
+    /*3) Dado um vetor que guarda o valor de faturamento diário de uma distribuidora, faça um programa,
+    na linguagem que desejar, que calcule e retorne:
+• O menor valor de faturamento ocorrido em um dia do mês;
+• O maior valor de faturamento ocorrido em um dia do mês;
+• Número de dias no mês em que o valor de faturamento diário foi superior à média mensal.*/
+
+
+
+    /*4) Dado o valor de faturamento mensal de uma distribuidora, detalhado por estado:
+SP – R$67.836,43
+RJ – R$36.678,66
+MG – R$29.229,88
+ES – R$27.165,48
+Outros – R$19.849,53
+Escreva um programa na linguagem que desejar onde calcule o percentual de representação que cada
+ estado teve dentro do valor total mensal da distribuidora.*/
+
+    val sp = 67.836,43
+    val rj = 36.678,66
+    val mg = 29.229,88
+    val es = 27.165,48
+    val outros = 19.849,53
+
+    var soma = sp+rj+mg+es+outros
+
+
+
+
+    /*5) Escreva um programa que inverta os caracteres de um string.
+IMPORTANTE:
+a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser
+previamente definida no código;
+b) Evite usar funções prontas, como, por exemplo, reverse;
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
